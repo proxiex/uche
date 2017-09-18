4382e2a07c02fa0b002d231cfc2f8383518ce6eb
samuel <difrentecho@gmail.com>
2017-09-18 13:20:40 +0100

    sets up server

---
+++ server/bin/www.js
@@ -0,0 +1,11 @@
+import * as http from 'http';
+import app from '../app';
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('Server is up and running on port: '+ port +'...');
+ 
